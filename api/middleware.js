3949f1e81d28ad47a1b68d9a5ef042d410524ebb
tiencoffee <tiencoffee4@gmail.com>
2021-02-23 01:10:00 +0700

    test 4

--- api/middleware.js
+++ api/middleware.js
@@ -1,18 +1,17 @@
-const https = require('https')
+const
+	https = require('https'),
+	url = require('url')
 
 module.exports = function(req, res, next) {
-	var {url} = req
-	if (url.startsWith('/api/')) {
-		params = new URLSearchParams(url)
-		fileUrl = params.get('url')
+	if (req.url.startsWith('/api/middleware.js/')) {
+		var fileUrl = req.url.replace('/api/middleware.js/', '')
 		https.get(fileUrl).on('response', response => {
 			var body = []
 			response.on('data', chunk => {
 				body.push(chunk)
 			})
 			response.on('end', () => {
-				buf = Buffer.concat(body)
-				res.end(buf)
+				res.end(Buffer.concat(body))
 			})
 			response.on('error', err => {
 				res.status(err.code).end(err.message)
